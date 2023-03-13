d2da53a6c4f6e314073c09f985a706d66b859623
SergeyB <brynkin.dev@gmail.com>
2023-03-13 05:43:17 +0300

    remove hover on calculator buttons in constructor mode

--- src/components/CalculatorConstructor/components/Widget/components/CalculatorButton/CalculatorButton.tsx
+++ src/components/CalculatorConstructor/components/Widget/components/CalculatorButton/CalculatorButton.tsx
@@ -1,5 +1,7 @@
 import './CalculatorButton.scss'
 import {CalculatorButtonType} from "../../../../../../constants/types";
+import {useSelector} from "react-redux";
+import {RootState} from "../../../../../../redux/store";
 
 interface CalculatorButtonProps {
     buttonType: CalculatorButtonType;
@@ -9,7 +11,10 @@ interface CalculatorButtonProps {
 }
 
 export const CalculatorButton = ({buttonType, value, height, width}: CalculatorButtonProps) => {
-    return <button className={`calculator-button calculator-button_${buttonType} height-${height}px width-${width}px`}>
+    const mode = useSelector((state: RootState) => state.calculator.mode)
+
+    return <button
+        className={`calculator-button calculator-button_${buttonType}${mode === 'runtime' ? ' hover' : ''} height-${height}px width-${width}px`}>
         <div>{value}</div>
     </button>
 }
