ce6c031e563b031b1eedaf23f201b5292c12fed8
SergeyB <brynkin.dev@gmail.com>
2023-03-16 03:05:36 +0300

    refactor calculator logic

--- src/redux/calculatorSlice.ts
+++ src/redux/calculatorSlice.ts
@@ -10,6 +10,7 @@ interface CalculatorState {
     result: string;
     displayState: DisplayStateEnum;
     currentOperation: OperationsEnum;
+    cashOperation: OperationsEnum;
 }
 
 const initialState: CalculatorState = {
@@ -20,6 +21,7 @@ const initialState: CalculatorState = {
     result: '',
     displayState: DisplayStateEnum.ShowFirstDigit,
     currentOperation: OperationsEnum.Empty,
+    cashOperation: OperationsEnum.Empty
 }
 
 export const calculatorSlice = createSlice({
@@ -43,41 +45,62 @@ export const calculatorSlice = createSlice({
             const index = state.availableWidgets.indexOf(action.payload);
             if (index > -1) state.availableWidgets.splice(index, 1);
         },
+
         addDigit: (state, action: PayloadAction<{ digit: number }>) => {
             if (state.displayState === DisplayStateEnum.ShowResult) state.displayState = DisplayStateEnum.ShowFirstDigit;
             if (state.currentOperation !== OperationsEnum.Empty) state.displayState = DisplayStateEnum.ShowSecondDigit;
+
             const {digit} = action.payload;
             const {displayState, firstDigit, secondDigit} = state
+
             if (displayState === DisplayStateEnum.ShowSecondDigit) {
-                if (state.secondDigit === '0') return
-                state.secondDigit = (secondDigit + digit).toString();
+                if (state.secondDigit === '0') {
+                    state.secondDigit = (digit).toString();
+                } else {
+                    state.secondDigit = (secondDigit + digit).toString();
+                }
             } else {
-                if (state.firstDigit === '0') return
-                state.firstDigit = (firstDigit + digit).toString();
+                if (state.firstDigit === '0') {
+                    state.firstDigit = (digit).toString();
+                } else {
+                    state.firstDigit = (firstDigit + digit).toString();
+                }
             }
         },
+
         setOperation: (state, action: PayloadAction<{ operation: OperationsEnum }>) => {
             if (state.displayState === DisplayStateEnum.ShowResult) state.firstDigit = state.result;
+            state.cashOperation = action.payload.operation;
             state.currentOperation = action.payload.operation;
         },
+
         calculate: (state) => {
+            if (state.displayState === DisplayStateEnum.ShowResult) state.firstDigit = state.result;
             if (state.secondDigit === '') state.secondDigit = state.firstDigit;
-            const {currentOperation, firstDigit, secondDigit} = state
+            if (state.currentOperation === OperationsEnum.Empty) state.currentOperation = state.cashOperation;
+
+            const currentOperation = state.currentOperation;
+            const firstDigit = +state.firstDigit;
+            const secondDigit = +state.secondDigit;
+
             switch (currentOperation) {
                 case OperationsEnum.Adding:
-                    state.result = (+firstDigit + +secondDigit).toString();
+                    state.result = (firstDigit + secondDigit).toString();
                     break;
                 case OperationsEnum.Subtraction:
-                    state.result = (+firstDigit - +secondDigit).toString();
+                    state.result = (firstDigit - secondDigit).toString();
                     break;
                 case OperationsEnum.Multiplication:
-                    state.result = (+firstDigit * +secondDigit).toString();
+                    state.result = (firstDigit * secondDigit).toString();
                     break;
                 case OperationsEnum.Division:
-                    state.result = (+firstDigit / +secondDigit).toString();
+                    state.result = (firstDigit / secondDigit).toString();
                     break;
+                case OperationsEnum.Empty:
+                    state.result = firstDigit.toString();
             }
-            state.result = state.result.length > 9 ? (+state.result).toFixed(9 - state.result.toString().split('.')[0].length) : state.result
+
+            state.result = (+state.result.split('.')[1] > 15 ? (+state.result).toFixed(15) : state.result).toString()
             if (!Number.isFinite(+state.result)) state.result = 'Не определено';
             state.firstDigit = '';
             state.secondDigit = '';
